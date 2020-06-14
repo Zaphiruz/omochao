bf357de09febb120d077bdb809da18e281689d0b
Zaphiruz <Jacobthomas428@gmail.com>
2020-06-14 10:31:31 -0700

    rework logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,15 +2,22 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
     level: 'debug',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
     transports: [
       //
       // - Write to all logs with level `info` and below to `combined.log` 
       // - Write all logs error (and below) to `error.log`.
       //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' })
+      new winston.transports.File({ filename: 'error.log', level: 'error', maxsize: 10000 }),
+      new winston.transports.File({ filename: 'combined.log', maxsize: 10000 }),
+
+      new winston.transports.Console({
+        stderrLevels: ['error'],
+        consoleWarnLevels : ['warn']
+      })
     ]
 });
 
